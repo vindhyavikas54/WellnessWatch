2d6104dc71df6ba8b85a090154f81efc16302eb3
vindhyavikas54 <159005819+vindhyavikas54@users.noreply.github.com>
2025-03-23 13:34:36 +0530

    Update app.py

--- Heart Disease predictor/app.py
+++ Heart Disease predictor/app.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
+import sklearn
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
